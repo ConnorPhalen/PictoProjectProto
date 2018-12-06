b3367f9c963da1a913cfacfb83cd1b2e9e005a4f
Connor Phalen <phalen.connor@hotmail.co.uk>
2018-12-06 12:05:58 -0800

    Slightly expanded upon the PictoCanvas class. Mainly modified to expand upon GitHub.

--- app/src/main/java/com/example/phale/pictoprojectproto/CoreClasses/PictoCanvas.java
+++ app/src/main/java/com/example/phale/pictoprojectproto/CoreClasses/PictoCanvas.java
@@ -1,5 +1,6 @@
 package com.example.phale.pictoprojectproto.CoreClasses;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Path;
@@ -14,9 +15,12 @@ import java.util.Dictionary;
 public class PictoCanvas extends Canvas {
 
     Dictionary<Path,Color> pathList;
+    Bitmap mBitmap;
 
-    public PictoCanvas()
+    // Takes in a Bitmap used to draw to when used with the canvas.
+    public PictoCanvas(Bitmap bitmap)
     {
-
+        super(bitmap);
+        mBitmap = bitmap;
     }
 }
